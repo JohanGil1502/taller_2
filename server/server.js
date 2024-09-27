c9924b217cb4f6f9411908229c68dbf68040c885
JohanGil1502 <johan.gil@uptc.edu.co>
2024-09-26 21:22:48 -0500

    Commit final

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const port = 4001;
+const port = process.env.PORT;
+
+
 
 app.use(cors());
 app.use(express.json());
